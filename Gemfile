2ecfa681161520ad29e548040e96ad3253ad46cd
Michael Lynch <gamegenius86@gmail.com>
2013-10-18 11:34:24 -0400

    fix gem file and installed foundation

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.0.0'
+ruby '2.0.0'
 
+gem 'rails', '4.0.0'
 gem 'pg'
+gem 'unicorn'
 gem 'high_voltage'
 gem 'haml-rails'
 gem 'zurb-foundation'
@@ -11,12 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 
-gem 'turbolinks'
-
-gem 'jbuilder', '~> 1.2'
-
 group :doc do
-
   gem 'sdoc', require: false
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,70 +1,21 @@
 SolutionsFuse::Application.routes.draw do
   root to: 'high_voltage/pages#show', id: 'index'
 
-  get '/products/19' => 'high_voltage/pages#show', id: '19'
-  get '/products/31' => 'high_voltage/pages#show', id: '31'
-  get '/products/52' => 'high_voltage/pages#show', id: '52'
-  get '/products/53' => 'high_voltage/pages#show', id: '53'
-  get '/products/C2' => 'high_voltage/pages#show', id: 'C2'
-  get '/products/F9' => 'high_voltage/pages#show', id: 'F9'
-  get '/products/H2' => 'high_voltage/pages#show', id: 'H2'
-  get '/products/SX2' => 'high_voltage/pages#show', id: 'SX2'
-  get '/products/F2-A1' => 'high_voltage/pages#show', id: 'F2-A1'
-  get '/products/JT-B1' => 'high_voltage/pages#show', id: 'JT-B1'
-  get '/products/FZ-G1' => 'high_voltage/pages#show', id: 'FZ-G1'
+  get '/products/19' => 'high_voltage/pages#show', id: 'products/19'
+  get '/products/31' => 'high_voltage/pages#show', id: 'products/31'
+  get '/products/52' => 'high_voltage/pages#show', id: 'products/52'
+  get '/products/53' => 'high_voltage/pages#show', id: 'products/53'
+  get '/products/C2' => 'high_voltage/pages#show', id: 'products/C2'
+  get '/products/F9' => 'high_voltage/pages#show', id: 'products/F9'
+  get '/products/H2' => 'high_voltage/pages#show', id: 'products/H2'
+  get '/products/SX2' => 'high_voltage/pages#show', id: 'products/SX2'
+  get '/products/F2-A1' => 'high_voltage/pages#show', id: 'products/F2-A1'
+  get '/products/JT-B1' => 'high_voltage/pages#show', id: 'products/JT-B1'
+  get '/products/FZ-G1' => 'high_voltage/pages#show', id: 'products/FZ-G1'
 
-  get '/case-studies/harley' => 'high_voltage/pages#show', id: 'harley'
-  get '/case-studies/amica' => 'high_voltage/pages#show', id: 'amica'
-  get '/case-studies/toledo' => 'high_voltage/pages#show', id: 'toledo'
-  get '/case-studies/nswc' => 'high_voltage/pages#show', id: 'nswc'
-  get '/case-studies/volvo' => 'high_voltage/pages#show', id: 'volvo'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get '/case-studies/harley' => 'high_voltage/pages#show', id: 'case-studies/harley'
+  get '/case-studies/amica' => 'high_voltage/pages#show', id: 'case-studies/amica'
+  get '/case-studies/toledo' => 'high_voltage/pages#show', id: 'case-studies/toledo'
+  get '/case-studies/nswc' => 'high_voltage/pages#show', id: 'case-studies/nswc'
+  get '/case-studies/volvo' => 'high_voltage/pages#show', id: 'case-studies/volvo'
 end
